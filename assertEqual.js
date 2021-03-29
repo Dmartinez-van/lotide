799ef6158a6981d06e08a92ae463dd6826812dd9
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-03-29 13:22:23 -0700

    Assertion function comparing two values

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀🟢 Assertion Passed: [" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("🤬🔴 Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("David", "david");
+assertEqual("avacado", 1);
+assertEqual("Tomato", "Tomato");
+assertEqual(1.5, 1.5000);
