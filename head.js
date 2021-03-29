ba319b29f374be30160175bb496d7893d725a444
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-03-29 14:57:43 -0700

    Commented out some test code

--- head.js
+++ head.js
@@ -11,12 +11,12 @@ const head = function(item) {
 };
 
 // Test Code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("David", "david");
-assertEqual("avacado", 1);
-assertEqual("Tomato", "Tomato");
-assertEqual(1.5, 1.5000);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("David", "david");
+// assertEqual("avacado", 1);
+// assertEqual("Tomato", "Tomato");
+// assertEqual(1.5, 1.5000);
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
