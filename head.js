2b9fa67dfc902b33b02f4b190a4c6d15d2c161ab
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-03-29 13:51:28 -0700

    Added head.js

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🤬🔴 Assertion Failed: [${actual}] === [${expected}]`);
+  }
+};
+
+const head = function(item) {
+  return item[0];
+};
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("David", "david");
+assertEqual("avacado", 1);
+assertEqual("Tomato", "Tomato");
+assertEqual(1.5, 1.5000);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Cat");
