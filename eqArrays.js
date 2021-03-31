83cd49a82122f307055f758d344ef0349a3fc37d
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-03-30 18:08:12 -0700

    Created flatten.js / updated eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -7,18 +7,20 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  let isTrue = false;
+  // If arrays are not same length, they are not the exact same.
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check same indicies between the arrays, must be exactly equal to pass.
   let i = 0;
   while (i < arr1.length) {
-    if (arr1[i] === arr2[i]) {
-      isTrue = true;
-    } else {
-      isTrue = false;
-      return isTrue;
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
     i++;
   }
-  return isTrue;
+  return true;
 };
 
 // Test Code
---
+++ flatten.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😀🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🤬🔴 Assertion Failed: [${actual}] === [${expected}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  // If arrays are not same length, they are not the exact same.
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check same indicies between the arrays, must be exactly equal to pass.
+  let i = 0;
+  while (i < arr1.length) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+const flatten = function(nestedArr) {
+
+  // Check if the parameter is indeed an array
+  if (!Array.isArray(nestedArr)) {
+    return null;
+  }
+
+  // Loop through each element, checking if each element is also an array.
+  // If it is an Array, bring the elements of which out and insert it into the outter array at the same index
+  
+  for (let i = 0; i < nestedArr.length; i++) {
+    if (Array.isArray(nestedArr[i])) {
+      for (let j = nestedArr[i].length - 1; j > -1; j--) {
+        nestedArr.splice(i + 1, 0, nestedArr[i][j]);
+      }
+      nestedArr.splice(i, 1);
+    }
+  }
+  return nestedArr;
+};
+
+console.log(flatten([1,[2,3,"Hello"],4,5,[6,7,8,9]]));
