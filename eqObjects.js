a85d6d5528a09a9fbb113acae9015d56951b2088
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-05 21:55:56 -0700

    Updated eqObjects to equate nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -1,32 +1,24 @@
-const eqArrays = require('./eqArrays');
-
 // Returns true if both objects have identifcal keys with identical values.
-// Otherwise you get back a big far false!
+// Otherwise you get back a big fat false!
 const eqObjects = function(obj1, obj2) {
   // Check for number of keys in each object
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   }
-
-  // We will loop over each key in obj1.
-  // And compare each key's value to every key:value pair in obj2.
-  // If a pair does not exsist, it'll be false.
   for (const key of Object.keys(obj1)) {
-    // This works because IF there is a key in obj1 AND obj2 we can call upon each
-    // value using the brackey notation below and compare them.
-    // If the key exsists in obj1 and NOT in obj2, it'll be false right away.
-    
-    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-      // If arrays are not perfectly equal, return false
-      if (!eqArrays(obj1[key], obj2[key])) {
-        return false;
-      }
-    } else if (obj1[key] !== obj2[key]) {
-      return false;
+    if (!Array.isArray(obj1[key]) && !Array.isArray(obj2[key])) {
+      return eqObjects(obj1[key], obj2[key]);
     }
   }
   // If the above code does not result in false, then the objects are equal
   return true;
 };
+
+//Test code
+// console.log(eqObjects({ a: { z: {cheese: 0} }, b: {poop: 'wow'} }, { a: { z: {cheese: 0} }, b: {poop: 'wow'} })); // => true
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+
+
 
 module.exports = eqObjects;
